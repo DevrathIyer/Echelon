ee5b1bb7249daf6544af4baa7b554023f2054199
Devrath Iyer <idevrath@gmail.com>
2016-11-25 12:08:48 -0700

    Commit

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name = 'home'),
     url(r'^/signedin', views.signedin, name = 'signedin'),
-    url(r'^/user/(?P<UserID>[a-z0-9]+)',views.user, name = 'user'),
+    #url(r'^/user/(?P<UserID>[a-z0-9]+)',views.user, name = 'user'),
 ]

ca0d89eca04aa45d35e279abf4d1cc3173fbb656
Devrath Iyer <idevrath@gmail.com>
2016-11-26 21:09:34 -0700

    Commit:

--- tracker/views.py
+++ tracker/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.db import models
 #from .models import  EchelonUser
 from random import randint
+import bcrypt
 import requests
 
 def home(request):
@@ -9,17 +10,18 @@ def home(request):
 
 def signedin(request):
     id_token = request.POST.get('TokenID')
-    email = request.POST.get('Email')
     GoogleID = request.POST.get('googleid')
     url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token="+id_token
     response = requests.get(url)
     if response.json()['aud'] == GoogleID:
         try:
+            UserID = response.json()['sub']
             #LoggedInUser = EchelonUser.objects.get(user_email=email)
             #userUrl = 'https://echelonportal.herokuapp.com/tracker/user/' + models.b64encode(LoggedInUser.user_index)
             #return redirect(userUrl)
         except:
-            return NewUser(request,email)
+            #return NewUser(request,email)
+            UserID = 0
 
     else:
         return render(request, 'tracker/faliure.html', {})

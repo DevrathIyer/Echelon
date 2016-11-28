43158a9db181d4a51d340428bdeb6f2122e0499b
Dhruv Iyer <iyer.dhruv99@gmail.com>
2016-11-27 23:47:08 -0700

    new RSA

--- Desktop/Echelon/app.js
+++ Desktop/Echelon/app.js
@@ -11,11 +11,13 @@ var bodyParser = require('body-parser');
 const Aerospike = require('aerospike');
 var bcrypt = require('bcrypt-nodejs');
 var crypto = require('crypto');
+var path = require('path');
+var fs = require('fs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
-const SERVER_PUBLIC_KEY="MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4bstItI4kdHNqONsukB2l4fT2OdpurLNwsjvYEC3boN7Q8PfPGNB1fnyAPBa63yZYjcjK7QmIsLehrb+0vURmWFNiPGUIvsfL/rJlQ3Vga2hlyy0sH33JluYrKnZQ04UuTmzk0eUvMyPVl9kQMWkeX7YwrC52FlL6wxYVgzxNC/rUJYDVfpd+fp9Lq+12fFxDtcB1+5dy6mWD9GCasxhbX1dbMkCftpeDnvvIQ1aYySmRKSD3ZkOccXOo2NaAyHqByR2KICcymvP3uvr622WQfNjPFhkoZLo4GQ/Aa+Lk8/KKHHhHhBI9k9LSJlMRpktO1oLUY+Zt77Aob7smmnAuwIDAQAB";
+//const SERVER_PUBLIC_KEY="MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4bstItI4kdHNqONsukB2l4fT2OdpurLNwsjvYEC3boN7Q8PfPGNB1fnyAPBa63yZYjcjK7QmIsLehrb+0vURmWFNiPGUIvsfL/rJlQ3Vga2hlyy0sH33JluYrKnZQ04UuTmzk0eUvMyPVl9kQMWkeX7YwrC52FlL6wxYVgzxNC/rUJYDVfpd+fp9Lq+12fFxDtcB1+5dy6mWD9GCasxhbX1dbMkCftpeDnvvIQ1aYySmRKSD3ZkOccXOo2NaAyHqByR2KICcymvP3uvr622WQfNjPFhkoZLo4GQ/Aa+Lk8/KKHHhHhBI9k9LSJlMRpktO1oLUY+Zt77Aob7smmnAuwIDAQAB";
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -201,6 +203,8 @@ router.route('/admin/getCredits').get(function(req, res)
 
 router.route('/admin/test').get(function(req, res)
 {
+  var abpath = path.resolve("public.pem");
+  var publickey = fs.readFileSync(abpath, "utf8");
   var buffer = new Buffer("hello world");
   var encrypted = crypto.publicEncrypt(SERVER_PUBLIC_KEY,buffer);
   var test = encrypted.toString("base64");

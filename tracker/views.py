c363b99cf7ca45639cd3155963a55ce7ef06d15c
Dave Fyre <Dave Fyre>
2016-12-02 13:56:47 -0700

    Commit

--- tracker/views.py
+++ tracker/views.py
@@ -10,7 +10,7 @@ def home(request):
 
 def signedin(request):
     id_token = request.POST.get('TokenID')
-    GoogleID = request.POST.get('googleid')
+    GoogleID = "867858739826-0j8s1vplsccuqcha9tng77pmrpc49mam.apps.googleusercontent.com"
     url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token="+id_token
     response = requests.get(url)
     if response.json()['aud'] == GoogleID:

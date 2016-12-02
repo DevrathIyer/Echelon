dabf07d9ddb143289fce9209c69ab47246ddf530
Dhruv Iyer <iyer.dhruv99@gmail.com>
2016-12-02 16:04:29 -0700

    Committed at: +Fri Dec  2 16:04:29 MST 2016

--- Desktop/Echelon/app.js
+++ Desktop/Echelon/app.js
@@ -92,7 +92,7 @@ router.route('/api/train').get(function(req,res)
 
 router.route('/admin/userops/addCredits').post(function(req, res)
 {
-  res.json({"message":"Routed!"});
+  res.json({"message":"Admin access!"});
 });
 
 router.route('/admin/userops/check-user-exists').post(function(req, res)

92b83709f23ca3f2dd34181a8dd3e3acf484fa67
Dave Fyre <Dave Fyre>
2016-12-03 14:24:15 -0700

    Commit

--- tracker/views.py
+++ tracker/views.py
@@ -13,6 +13,7 @@ def signedin(request):
     id_token = request.POST.get('TokenID')
     GoogleID = "867858739826-0j8s1vplsccuqcha9tng77pmrpc49mam.apps.googleusercontent.com"
     url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token="+id_token
+    response = requests.get(url)
     if response.json()['aud'] == GoogleID:
         try:
             UserID = response.json()['sub']

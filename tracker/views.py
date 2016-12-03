6d38b23897cce3105d45366cddf315f7deec57d2
Dave Fyre <Dave Fyre>
2016-12-03 14:35:44 -0700

    Commit

--- tracker/views.py
+++ tracker/views.py
@@ -17,14 +17,14 @@ def signedin(request):
     if response.json()['aud'] == GoogleID:
         try:
             #UserID = response.json()['sub']
-            return render(request, 'tracker/faliure.html', {'JSON': response.json()})
+            return render(request, 'tracker/Faliure.html', {'JSON': response.json()})
             #LoggedInUser = EchelonUser.objects.get(user_email=email)
             #userUrl = 'https://echelonportal.herokuapp.com/tracker/user/' + models.b64encode(LoggedInUser.user_index)
             #return redirect(userUrl)
         except:
-            return render(request, 'tracker/faliure.html', {'JSON': response.json()})
+            return render(request, 'tracker/Faliure.html', {'JSON': response.json()})
             #return NewUser(request,email)
             UserID = 0
 
     else:
-        return render(request, 'tracker/faliure.html', {})
+        return render(request, 'tracker/Faliure.html', {})

7838e148c1ae73125dc0a1e9c83a356d60e4687d
Dhruv Iyer <iyer.dhruv99@gmail.com>
2016-12-09 16:20:36 -0700

    Committed at: Fri Dec  9 16:20:36 MST 2016

--- Desktop/Echelon/app.js
+++ Desktop/Echelon/app.js
@@ -247,6 +247,7 @@ router.route('admin/userops/createNewProject').post(function(req, res)
 router.route('/admin/test').get(function(req, res)
 {
   var text = req.query.text;
+  res.json({"message":text});
   var client_buffer = new Buffer(text, "base64");
   var client_decrypted = crypto.publicDecrypt(CLIENT_PUBLIC_KEY, client_buffer);
   console.log(client_decrypted.toString("utf8"));

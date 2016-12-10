78e6c7d4691efd09efc82f6ee2d75a37b370a08a
Dhruv Iyer <iyer.dhruv99@gmail.com>
2016-12-09 17:00:59 -0700

    Committed at: Fri Dec  9 17:00:59 MST 2016

--- Desktop/Echelon/app.js
+++ Desktop/Echelon/app.js
@@ -258,7 +258,7 @@ router.route('/admin/test').get(function(req, res)
   };
   var encrypted = crypto.privateEncrypt(privateClientKey, client_buffer);
 
-  var buffer = new Buffer(encrypted);
+  var buffer = new Buffer(encrypted, "base64");
   var client_decrypted = crypto.publicDecrypt(CLIENT_PUBLIC_KEY, buffer);
   console.log(client_decrypted.toString("utf8"));
   //res.json({"message":client_decrypted.toString("utf8")});

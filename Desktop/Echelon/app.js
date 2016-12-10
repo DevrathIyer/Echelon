d26f438776dc4bfcd889f94a5237a35bbaed545b
Dhruv Iyer <iyer.dhruv99@gmail.com>
2016-12-09 18:05:11 -0700

    Committed at: Fri Dec  9 18:05:11 MST 2016

--- Desktop/Echelon/app.js
+++ Desktop/Echelon/app.js
@@ -247,7 +247,7 @@ router.route('admin/userops/createNewProject').post(function(req, res)
 router.route('/admin/test').get(function(req, res)
 {
   var text = req.query.text;
-  
+  console.log(text);
  //res.json({"message":text});
   var client_buffer = new Buffer(text, "base64");
   var pathToPrivateClientKey = path.resolve("EchelonClientKeys/private.pem");

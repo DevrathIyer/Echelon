eb658ba8f2f8f6d6680949ca88b184799cb6cdf6
Dave Fyre <Dave Fyre>
2016-12-23 14:54:38 -0700

    Commit

--- tracker/views.py
+++ tracker/views.py
@@ -51,10 +51,13 @@ YZwZkOfXiUuP0/8ff94r4B23WE3kAxJXj09IiANe6aX9WJtcGNbhqCNU9hgRMu2h
     string = 'hello' #base64.b64encode(bytes('hello'))
     hash = SHA256.new(string).digest()
     data = r.sign(hash, 32)
-    post_data = {'auth':os.environ['password'], 'uid':''}
     if response.json()['iss'] in ('accounts.google.com', 'https://accounts.google.com'):
         if response.json()['aud'] == GoogleID:
             #response['auth'] = os.environ['password']
+            userid = response.json()['sub']
+            name = response.json()['name']
+            email = response.json()['email']
+            post_data = {'auth': os.environ['password'], 'uid': userid, 'name': name , 'email': email}
             response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/createUser', data=post_data)
             return render(request, 'tracker/Faliure.html', {'JSON': response.json()})
         else:

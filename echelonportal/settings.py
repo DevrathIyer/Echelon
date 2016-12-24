6a7522ee74803cb2f4348df16160d29ecaadf650
Dave Fyre <Dave Fyre>
2016-12-24 10:46:48 -0700

    Commit

--- echelonportal/settings.py
+++ echelonportal/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    'home.apps.HomeConfig'
+    'home.apps.HomeConfig',
     'tracker.apps.TrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',

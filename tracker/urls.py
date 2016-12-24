f086e9a2dd5e19de7cd07d721d674997557e7094
Dave Fyre <Dave Fyre>
2016-12-23 21:09:24 -0700

    Commit

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.home, name = 'home'),
     url(r'^login', views.login, name = 'login'),
     url(r'^signup', views.signup, name = 'signup'),
+    url(r'^newuser', views.createnewuser, name = 'createnewuser'),
     url(r'^signedin', views.viewuserdata, name = 'viewuserdata'),
     #url(r'^createnewuser', views.createnewuser, name = 'createnewuser'),
     #url(r'^/user/(?P<UserID>[a-z0-9]+)',views.user, name = 'user'),

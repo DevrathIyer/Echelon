031773142288985a0766be0f279d9a6d9408685c
Dave Fyre <idevrath@gmail.com>
2016-12-29 23:43:16 -0700

    Commit

--- tracker/views.py
+++ tracker/views.py
@@ -105,14 +105,15 @@ def viewuserdata(request):
                 post_data = {'auth': os.environ['password'], 'uid': userid}
                 response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/getUserProjects', data=post_data)
                 Projects = response.json()['project_list'].split()
-                ProjectNumber = len(Projects)-1
+                ProjectNumber = len(Projects)
                 ProjectList = ['' for x in range(ProjectNumber)]
                 for x in range(ProjectNumber):
-                    post_data = {'auth': os.environ['password'], 'projectid': Projects[x]}
-                    response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/getProjectInfo',
+                    if(x != 0):
+                        post_data = {'auth': os.environ['password'], 'projectid': Projects[x]}
+                        response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/getProjectInfo',
                                                  data=post_data)
-                    ProjectList[x] = response.json()
-                return render(request, 'tracker/Projects.html', {'Projects': ProjectList,'Number':range(ProjectNumber),'UserName':UserName})
+                        ProjectList[x-1] = response.json()
+                return render(request, 'tracker/Projects.html', {'Projects': ProjectList,'Number':range(ProjectNumber)'UserName':UserName})
         else:
             return render(request, 'tracker/Faliure.html', {})
     else:

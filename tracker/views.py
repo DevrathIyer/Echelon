954e50a41b29b89efe172b3e4d7f4a6e383ce0bb
Dave Fyre <Dave Fyre>
2017-01-04 14:14:47 -0700

    Commit

--- tracker/views.py
+++ tracker/views.py
@@ -78,6 +78,9 @@ def credits(request):
                 UserCredits = response.json()['credits']
                 return render(request, 'tracker/Credits.html', {'Credits':UserCredits})
 
+def layeredit(request):
+    try:
+        Newlayers = request.POST.get('')
 def createnewuser(request):
     id_token = request.POST.get('TokenID')
     GoogleID = "867858739826-0j8s1vplsccuqcha9tng77pmrpc49mam.apps.googleusercontent.com"

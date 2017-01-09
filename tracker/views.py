0e25657bec73541e12a8d88ec04c9011bea4a150
Dave Fyre <idevrath@gmail.com>
2017-01-08 17:02:09 -0700

    Commit

--- tracker/views.py
+++ tracker/views.py
@@ -133,7 +133,7 @@ def viewuserdata(request):
                         response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/getProjectInfo',
                                                  data=post_data)
                         ProjectList[x] = response.json()
-                        ProjectList[x]['Neurons_per_Layer'] = response.json()['Neurons_per_Layer'].split(',')
+                        ProjectList['Neurons_per_Layer'][x] = response.json()['Neurons_per_Layer'].split(',')
                 ProjectList.pop(0)
                 NeuronLength = ProjectList['Neurons_per_Length']
                 return render(request, 'tracker/Projects.html', {'Projects': ProjectList,'UserName':UserName,'NeuronLength':NeuronLength})

56f243e6682e0730c8f7186d3dc51057f7ffff56
Dave Fyre <idevrath@gmail.com>
2017-05-26 19:26:58 -0700

    REVERT

--- tracker/views.py
+++ tracker/views.py
@@ -3,11 +3,47 @@ from django.db import models
 from django.http import HttpResponse,Http404
 import json
 from random import randint
-import bcrypt
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from Crypto import Random
 import base64
 import requests
 import os
-import random, string
+f = """-----BEGIN RSA PRIVATE KEY-----
+Proc-Type: 4,ENCRYPTED
+DEK-Info: DES-EDE3-CBC,2F5252FFFF575D73
+
+EAOm5SztxVoaNQgh0PyR0FZlyJM5Hdyz+GL/GVQyJjaUFMmOkDTUsNcv8k4AzB7V
+9yz0gpWajwQCqJDfC3u9FO8PtPKisqvB1/cy0QfAvODND6XapYotav94K8y2ape2
+2amfaifN0Nci/DnbU5t5trwRyh6A/iC2RVTFuqssFMFDUMagXbzgD56stb0n6ccF
+NqprkbNNN/ICaFG+YcwX0Pr5i2EaSiqP/6a9+seK0cf+/tU4Uc43Rbgzj8jxAWkM
+nb0ufGPmV0U2a5i3G7/4Vh/1/HQa8uLYDvANCpknuWyV1qvU8ez+KF2iSo+WNApi
+avEi785Rw8hzA1OOnxkYsPp1Klrj5xPMZBv45MF4PLji32K37BvFNhxM7svtotmi
+akyVmLLwBnSjqWW48e9s0iFMcreVKSwG03uCq/s4iXJFdxLPp3NVU3I+nb3BPdeV
+dQRsV6M/eDMsvhXuAx7d7wMkl6cB0rOxbZ2Gxmvwwx1tT70NtAJFtCPIkx3JDEOH
+bBynvMdKBP43cGfjSYpcnUtVlgRTCRFxiQgd8ozIDe+I7zUAhDFiLdksiHidM75O
+vB3AnkiLTmsesPixMABd6peL/d1dKDEe+yhr02mhwLFx/6aV4eRU2PTzJudHNHOD
+hJ5Dc2o0vDI/1hSY02boI5qqgllWfkB8zhSkcrn+o6nt59zPEidiuxPVkFtd+K15
+dzPNi+br/cLPatcQlDLlt/mNIK4LmJVzIGxgspvEN1hO2toh7gEBfQafMRo+o7Yo
+z5bAgAD303S3Hp2J0zb3F7UAP0GjlBO3dm+wDJJibSTVFeLOAPMNBQitHcxpAK/L
+lscegz/u/qyHvxwDNtlVzJcU1c7N6sgl22kM//t5NVumay1wztUeUoKblyu+Ejlw
+wMWYu/f789QanN81tWRVHVCdpH84UnRVEif0BvTIhho+zF7XHNaMrQCPbZwoz2O9
+GwbWcadgdKrGjfGO6nV2OPSMEL8q+4k2K2iaVQNWWPkFtmiT2v38mwBBuIt3UL7T
+t7sbwATQCg3+PmnBmX6NmYKUllzQfaN+0X74vuPXdEpTtS4AG/u5KL6KB0H/VzzN
+zMqVDV3IqoVZtDn3zE5ssh9I61g0U5FByq42AfAKKQd92Asw2bdmJoT+uMzOqbI1
+N0gZXIDIi1XWGjygv/9mIWyuljqLRwNPtRE59eNsb2MuQeCH9UBSUPWHHBfyNmpY
+WNQ8bjaHmASR0Kba9GugyHdVoNkNKDflSDpkr3VCiSOJewN58MujTOi4HVdgU05o
+2vExDYHBzV/wAZXiuNO8nd/bRRJKjhZ/IH3vusz/kzw9fNQjrcLTwb8TCceTvOlG
+ZpUSJbN/0oFtbGidplSODGdyRaZ9lHSNA4ylS4S9c1U8Hv/zC+ZHOa8OMAhS2CKG
+YA3OpZujVRdQPeodBCqFocvEjEwWhPzpxN/Q3/bMX9gvEZNSjl2N5ms7EkxRqQmA
+2GwN4EvxUrfa+xUMXd1mPz0h2V5vY0RUhiZx08DcTE2yHw5jf3kf758tgUb/e3EH
+YZwZkOfXiUuP0/8ff94r4B23WE3kAxJXj09IiANe6aX9WJtcGNbhqCNU9hgRMu2h
+-----END RSA PRIVATE KEY-----"""
+r = RSA.importKey(f, passphrase=os.environ['ENCRYPTION_PASSWORD'])
+os.environ['ENCRYPTION_PASSWORD']
+string = 'hello'  # base64.b64encode(bytes('hello'))
+hash = SHA256.new(string).digest()
+data = r.sign(hash, 32)
 
 def home(request):
     return render(request, 'tracker/Homepage.html', {})
@@ -26,38 +62,12 @@ def signout(request):
     request.session.flush()
     return redirect('login')
 
-def addcredits(request):
-    try:
-        id_token = request.session['TokenID']
-    except:
-        return HttpResponse(json.dumps({'status': 'NAH'}), content_type='application/json')
-    GoogleID = "867858739826-0j8s1vplsccuqcha9tng77pmrpc49mam.apps.googleusercontent.com"
-    url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + id_token
-    response = requests.get(url)
-    if response.json()['iss'] in ('accounts.google.com', 'https://accounts.google.com'):
-        if response.json()['aud'] == GoogleID:
-            userid = response.json()['sub']
-            post_data = {'auth': os.environ['password'], 'uid': userid, 'numcredits': 1000}
-            response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/addCredits', data=post_data)
-            return HttpResponse(json.dumps({'message': response.json()['message']}), content_type='application/json')
-
-def checkproject(request):
+def checkprojectname(request):
     try:
         id_token = request.session['TokenID']
         projectid = request.POST.get('projectid')
     except:
-        return HttpResponse(json.dumps({'status': 'NAH'}), content_type='application/json')
-    GoogleID = "867858739826-0j8s1vplsccuqcha9tng77pmrpc49mam.apps.googleusercontent.com"
-    url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + id_token
-    response = requests.get(url)
-    if response.json()['iss'] in ('accounts.google.com', 'https://accounts.google.com'):
-        if response.json()['aud'] == GoogleID:
-            post_data = {'auth': os.environ['password'], 'projectid': projectid}
-            response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/getProjectInfo', data=post_data)
-            if (response.json()['message'] == 'could not get project info'):
-                return render(request, 'ProjectCheckAvailable.html', {})
-            else:
-                return render(request, 'ProjectCheckUsed.html', {})
+        return render(request, 'tracker/Faliure.html', {})
 
 def addproject(request):
     try:
@@ -76,38 +86,10 @@ def addproject(request):
             userid = response.json()['sub']
             post_data = {'auth': os.environ['password'], 'projectid': projectid}
             response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/getProjectInfo', data=post_data)
-            if (response.json()['message'] == 'could not get project info'):
-                key = ''.join(random.choice(string.lowercase) for i in range(20))
-                salt = bcrypt.gensalt()
-                apikey = bcrypt.hashpw(key,salt)
-                post_data = {'auth': os.environ['password'], 'projectid': projectid, 'numlayers': layers, 'nodes': neurons,
-                         'uid': userid, 'apikey': apikey}
-                response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/createNewProject', data=post_data)
-                return HttpResponse(json.dumps({'message': response.json()['message']}), content_type='application/json')
-            """
-            if (response.json()['message'] == 'could not get project info'):
-                post_data = {'auth': os.environ['password'], 'uid': userid}
-                response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/getUserProjects',data=post_data)
-                try:
-                    Projects = response.json()['project_list'].split()
-                except:
-                    Projects = ['']
-                ProjectNumber = len(Projects)
-                ProjectList = ['' for x in range(ProjectNumber)]
-                Neurons = ['' for x in range(ProjectNumber)]
-                NeuronLength = ['' for x in range(ProjectNumber)]
-                for x in range(ProjectNumber):
-                    if (x != 0):
-                        post_data = {'auth': os.environ['password'], 'projectid': Projects[x]}
-                        response = requests.post(
-                            'https://echelon-nn.herokuapp.com/admin/userops/getProjectInfo',data=post_data)
-                        ProjectList[x] = response.json()
-                        ProjectList[x]['Neurons_per_Layer'] = response.json()['Neurons_per_Layer'].split(',')
-                        ProjectList[x]['NeuronLength'] = len(response.json()['Neurons_per_Layer'].split(','))
-                        ProjectList.pop(0)
-                        return render(request, 'Example.html', {'Projects': ProjectList, 'ProjectID': projectid})
-    """
-
+            if(response.json()['message'] == 'could not get project info'):
+                return HttpResponse(json.dumps({'status':'available'}),content_type ='application/json')
+            else:
+                return HttpResponse(json.dumps({'status': 'used'}), content_type='application/json')
 
 def deleteproject(request):
     try:
@@ -166,24 +148,23 @@ def editproject(request):
             response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/getUserProjects', data=post_data)
             try:
                 Projects = response.json()['project_list'].split()
-                ProjectNumber = len(Projects)
-                ProjectList = ['' for x in range(ProjectNumber)]
-                Neurons = ['' for x in range(ProjectNumber)]
-                NeuronLength = ['' for x in range(ProjectNumber)]
-                for x in range(ProjectNumber):
-                    if (x != 0):
-                        post_data = {'auth': os.environ['password'], 'projectid': Projects[x]}
-                        response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/getProjectInfo',
-                                                 data=post_data)
-                        ProjectList[x] = response.json()
-                        ProjectList[x]['Neurons_per_Layer'] = response.json()['Neurons_per_Layer'].split(',')
-                        ProjectList[x]['NeuronLength'] = len(response.json()['Neurons_per_Layer'].split(','))
-                ProjectList.pop(0)
-                return render(request, 'Example.html',
-                              {'Projects': ProjectList, 'ProjectID': projectid, 'Number': ProjectNumber})
             except:
                 Projects = ['']
-                return HttpResponse('<b>ayyo</b>', content_type='application/html')
+            ProjectNumber = len(Projects)
+            ProjectList = ['' for x in range(ProjectNumber)]
+            Neurons = ['' for x in range(ProjectNumber)]
+            NeuronLength = ['' for x in range(ProjectNumber)]
+            for x in range(ProjectNumber):
+                if (x != 0):
+                    post_data = {'auth': os.environ['password'], 'projectid': Projects[x]}
+                    response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/getProjectInfo',
+                                             data=post_data)
+                    ProjectList[x] = response.json()
+                    ProjectList[x]['Neurons_per_Layer'] = response.json()['Neurons_per_Layer'].split(',')
+                    ProjectList[x]['NeuronLength'] = len(response.json()['Neurons_per_Layer'].split(','))
+            ProjectList.pop(0)
+            return render(request, 'Example.html', {'Projects': ProjectList,'ProjectID':projectid})
+            #return HttpResponse('<b>ayyo</b>', content_type='application/html')
     #return HttpResponse(json.dumps('nahhhh'), content_type='application/json')
 
 def credits(request):
@@ -216,7 +197,7 @@ def createnewuser(request):
             email = response.json()['email']
             post_data = {'auth': os.environ['password'], 'uid': userid, 'name': name , 'email': email}
             response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/createUser', data=post_data)
-            return render(request, 'tracker/Faliure.html', {'JSON': response.json()})
+            return render(request, 'tracker/NewUser.html', {'JSON': response.json()})
         else:
             return render(request, 'tracker/Faliure.html', {})
     else:
@@ -248,7 +229,7 @@ def viewuserdata(request):
                 Neurons = ['' for x in range(ProjectNumber)]
                 NeuronLength = ['' for x in range(ProjectNumber)]
                 for x in range(ProjectNumber):
-                    if (x != 0):
+                    if(x!=0):
                         post_data = {'auth': os.environ['password'], 'projectid': Projects[x]}
                         response = requests.post('https://echelon-nn.herokuapp.com/admin/userops/getProjectInfo',
                                                  data=post_data)
@@ -256,10 +237,7 @@ def viewuserdata(request):
                         ProjectList[x]['Neurons_per_Layer'] = response.json()['Neurons_per_Layer'].split(',')
                         ProjectList[x]['NeuronLength'] = len(response.json()['Neurons_per_Layer'].split(','))
                 ProjectList.pop(0)
-                return render(request, 'tracker/Projects.html',
-                              {'Projects': ProjectList, 'UserName': UserName, 'number': ProjectNumber})
-            else:
-                return render(request, 'tracker/Projects.html', {'JSON':response.json()['error']})
+                return render(request, 'tracker/Projects.html', {'Projects': ProjectList,'UserName':UserName})
         else:
             return render(request, 'tracker/Faliure.html', {})
     else:
